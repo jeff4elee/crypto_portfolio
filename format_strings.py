e99a78e04e2c2f4abd1d475eaf0de63464a7193b
jeff4elee <jeff4elee@gmail.com>
2017-05-13 03:50:12 -0700

    Initial commit

---
+++ format_strings.py
@@ -0,0 +1,8 @@
+USAGE = '\nUsage:\n\n./calc.py <coin> <amount_bought> <total_cost> \t[Inserts values into the portfolio]\n./calc.py -i <coin> \t\t\t\t[Inspects the info of a coin]\n./calc.py -c <coin> \t\t\t\t[Clears the coin from the portfolio]\n./calc.py -a \t\t\t\t\t[Checks the whole portfolio]\n'
+COINS_OWNED = "You currently own %.2f %s"
+COINS_SUMMARY = "You currently own %.2f %s, which is worth $%.2f as of now"
+COINS_TOTAL_VALUE = "Your %s is currently worth $%.2f"
+COINS_PROFIT = "If you cash out now, your expected earnings for %s is $%.2f"
+BREAK = "\n------------------------------------------------\n"
+TREND = "current price: $%.5f\n1h change: %.2f%%\n24h change: %.2f%%\n7d change: %.2f%%\n"
+PROMPT_COIN_DELETE = "Are you sure you want to delete %s off your portfolio? (y/n)"
